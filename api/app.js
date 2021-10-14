ab061fd698835fbca8cb99f28a2f713588d572b6
Grace Heffernan <gsxkat@gmail.com>
2021-10-14 19:57:47 +1000

    Bug fix

--- api/app.js
+++ api/app.js
@@ -18,11 +18,16 @@ var markovHexcodesRouter = require("./routes/markovhexcodes");
 
 var app = express();
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
