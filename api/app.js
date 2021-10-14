5823fa8772e096a1c36f3b0e30da63500d81acce
Grace Heffernan <gsxkat@gmail.com>
2021-10-14 20:04:01 +1000

    Minor bug fix

--- api/app.js
+++ api/app.js
@@ -19,7 +19,7 @@ var markovHexcodesRouter = require("./routes/markovhexcodes");
 var app = express();
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
